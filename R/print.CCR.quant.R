35fef7d6cea9db1e3bf0af200135fccd35ab291c
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-12-01 17:06:23 +0000

    changed printing method for ccr.quant.objects

--- R/print.CCR.quant.R
+++ R/print.CCR.quant.R
@@ -4,7 +4,8 @@
 print.CCR.quant <- function(CCR.quant.object)
 {
     msg <- "CCR ("
-    msg <- paste0(msg, CCR.quant.object$q*100, "% events)\n")
+    msg <- paste0(msg, CCR.quant.object$q*100, "% events in ")
+    msg <- paste0(msg, names(CCR.quant.object$estimate), " places)\n")
 
     cat(msg)
     print(CCR.quant.object$estimate)
