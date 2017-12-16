0a6acf3396fd0c92f93949568fb13566b91c952e
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-12-16 13:12:11 -0600

    bug in cumprop, removed if, always sort

--- R/cumprop.R
+++ R/cumprop.R
@@ -3,7 +3,8 @@
 
 cumprop <- function(x, decreasing = TRUE)
 {
-    if (is.unsorted(x)) x <- sort(x, decreasing = decreasing)
+    #if (is.unsorted(x)) x <- sort(x, decreasing = decreasing)
+    x <- sort(x, decreasing = decreasing)
     result <- cumsum(x)/sum(x)
 
     return(result)
