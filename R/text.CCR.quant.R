a50185d37c5e2c3d564aeff27cb0f52e2f287b8f
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-12-16 19:48:17 -0600

    added option to specify different positions to both annotations in text.ccr.quant method

--- R/text.CCR.quant.R
+++ R/text.CCR.quant.R
@@ -2,16 +2,16 @@
 #' @export
 #'
 
-text.CCR.quant <- function(CCR.quant.object, pos = 4, ...)
+text.CCR.quant <- function(CCR.quant.object, pos1 = 4, pos2 = 1, ...)
 {
     q <- CCR.quant.object$q
     est <- CCR.quant.object$estimate
     qest <- q/est
 
     label <- substitute(paste(Y[i], "=", q, "%"), list(q = q * 100))
-    text(x = qest, y = q, label, pos = pos, ...)
+    text(x = qest, y = q, label, pos = pos1, ...)
 
     label2 <- substitute(paste(Y*minute[Y[i]], "=", qest, "%"), list(qest = round(qest * 100, 2)))
-    text(x = qest, y = qest, label2, pos = pos, ...)
+    text(x = qest, y = qest, label2, pos = pos2, ...)
 
 }
