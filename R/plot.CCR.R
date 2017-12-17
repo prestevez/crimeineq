f0b0d6dc19f8c42230d505c5854efc9638ffac43
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-12-16 18:40:38 -0600

    simplified plot method for ccr objects

--- R/plot.CCR.R
+++ R/plot.CCR.R
@@ -12,21 +12,14 @@ plot.CCR <- function(CCR.object, ...)
     else
         central_measure <- median
 
-    indicator <- closest(CCR.object$ccrvec, central_measure(CCR.object$ccrvec))
-
-    Xi <- cumprop(CCR.object$expdist[[indicator]])
-    Xi <- c(0, Xi)
-
-
     n <- length(CCR.object$obs)
     c <- sum(CCR.object$obs)
 
     perfect <- rep(1, n)
-    cumperfect <- cumprop(perfect)
-    cumperfect <- c(0, cumperfect)
+    Xi <- cumprop(perfect)
+    Xi <- c(0, Xi)
 
 
-    plot(x = cumperfect, y = Yi, type = "l", xaxs = "i", yaxs = "i",
+    plot(x = Xi, y = Yi, type = "l", xaxs = "i", yaxs = "i",
               xlim = c(0,1), ylim = c(0,1), ...)
-    lines(x = cumperfect, y = Xi, lty = 2)
 }
