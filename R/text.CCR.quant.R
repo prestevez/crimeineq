3ca2a3c720d80e93746496514079b0e977441d5e
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-12-17 19:31:48 -0600

    added option to specify text labels in text.ccr.quant method

--- R/text.CCR.quant.R
+++ R/text.CCR.quant.R
@@ -2,18 +2,26 @@
 #' @export
 #'
 
-text.CCR.quant <- function(CCR.quant.object, pos1 = 4, pos2 = 4, ...)
+text.CCR.quant <- function(CCR.quant.object, pos1 = 4, pos2 = 4,
+                           label1 = NULL, label2 = NULL, ...)
 {
     q <- CCR.quant.object$q
     qinv <- CCR.quant.object$qinv
     est <- CCR.quant.object$estimate
     qest <- q/est
 
-    label <- substitute(paste(Y[i], "=", q, "%"), list(q = q * 100))
-    text(x = qinv, y = q, label, pos = pos1, ...)
+    if(is.null(label1))
+    {
+        label1 <- substitute(paste(Y[i], "=", q, "%"), list(q = q * 100))
+    }
 
-    label2 <- substitute(paste(Y*minute[paste(Y[i], "=", q, "%")], "=", qest, "%"),
-                                        list(qest = round(qest * 100, 2), q = q * 100))
+    if(is.null(label2))
+    {
+        label2 <- substitute(paste(Y*minute[paste(Y[i], "=", q, "%")], "=", qest, "%"),
+                             list(qest = round(qest * 100, 2), q = q * 100))
+    }
+
+    text(x = qinv, y = q, label1, pos = pos1, ...)
     text(x = qinv, y = qest, label2, pos = pos2, ...)
 
 }
