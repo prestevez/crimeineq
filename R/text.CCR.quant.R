30947138ae6171352d93200f8469cc71c1812643
Patricio Rodrigo Estévez Soto <prestevez@gmail.com>
2017-12-22 16:58:21 -0600

    changed text method for ccr.quant objects

--- R/text.CCR.quant.R
+++ R/text.CCR.quant.R
@@ -12,13 +12,15 @@ text.CCR.quant <- function(CCR.quant.object, pos1 = 4, pos2 = 4,
 
     if(is.null(label1))
     {
-        label1 <- substitute(paste(Y[i], "=", q, "%"), list(q = q * 100))
+        #label1 <- substitute(paste(Y[i], "=", q, "%"), list(q = q * 100))
+        label1 <- substitute(paste(q, "%"), list(q = q * 100))
     }
 
     if(is.null(label2))
     {
-        label2 <- substitute(paste(Y*minute[paste(Y[i], "=", q, "%")], "=", qest, "%"),
-                             list(qest = round(qest * 100, 2), q = q * 100))
+        #label2 <- substitute(paste(Y*minute[paste(Y[i], "=", q, "%")], "=", qest, "%"),
+        #                     list(qest = round(qest * 100, 2), q = q * 100))
+        label2 <- substitute(paste(qest, "%"), list(qest = round(qest * 100, 2)))
     }
 
     text(x = qinv, y = q, label1, pos = pos1, ...)
